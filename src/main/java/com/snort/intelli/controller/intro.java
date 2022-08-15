8f79ee2a0ccdb061c2da7f3387bde4a4a2ea9448
MausoofAzam <Azam_7899@outlook.com>
2022-08-15 11:26:49 +0530

    first commit

---
+++ src/main/java/com/snort/intelli/controller/intro.java
@@ -0,0 +1,36 @@
+package com.snort.intelli.controller;
+
+public class intro {
+
+	private int id;
+	private String name;
+	private String mob;
+	
+	
+	public intro(int id, String name, String mob) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.mob = mob;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMob() {
+		return mob;
+	}
+	public void setMob(String mob) {
+		this.mob = mob;
+	}
+	
+	
+}
